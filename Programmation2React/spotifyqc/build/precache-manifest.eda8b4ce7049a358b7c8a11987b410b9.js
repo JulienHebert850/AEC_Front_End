f41d5c5ccec402604eb7e697e5f4f61fd609cb50
JulienHebert850 <57509556+JulienHebert850@users.noreply.github.com>
2020-06-16 19:33:14 -0400

    add build

---
+++ Programmation2React/spotifyqc/build/precache-manifest.eda8b4ce7049a358b7c8a11987b410b9.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1d0260d3131041525792d4160096617",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2ef29613fef0e42add1",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a170afb40a5841d1516b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e2ef29613fef0e42add1",
+    "url": "/static/js/2.b8fa1ec9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.b8fa1ec9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a170afb40a5841d1516b",
+    "url": "/static/js/main.c26c5369.chunk.js"
+  },
+  {
+    "revision": "adb03825e7492a60b0d5",
+    "url": "/static/js/runtime-main.6f337dd4.js"
+  },
+  {
+    "revision": "656f121839ff6c2f7f8f633ce8b3994a",
+    "url": "/static/media/LogoSpotifyQc.656f1218.png"
+  },
+  {
+    "revision": "496d06f8546f65e4800c8810d454def8",
+    "url": "/static/media/PierreLapointeEnnui.496d06f8.jpg"
+  },
+  {
+    "revision": "32fb05281aa8063845705c7640e47b5e",
+    "url": "/static/media/PierreLapointeForetDesMalAimes.32fb0528.jpg"
+  },
+  {
+    "revision": "8079851e33bada75f8ff5d24693a5e26",
+    "url": "/static/media/PierreLapointePunkt.8079851e.jpg"
+  },
+  {
+    "revision": "fc7f1198e299f2a7a596526277caf2cc",
+    "url": "/static/media/PierreLapointeScienceCoeur.fc7f1198.jpg"
+  },
+  {
+    "revision": "ef793653ba54dbdc0923e4595346887a",
+    "url": "/static/media/SRVBluesYouCanUse.ef793653.jpg"
+  },
+  {
+    "revision": "b7bcc3ab3a99d09209e016c39b7a2a30",
+    "url": "/static/media/SRVCompleteEpicRecordings.b7bcc3ab.jpg"
+  },
+  {
+    "revision": "764271f3edec26b461191b63913b67e8",
+    "url": "/static/media/SRVStandTheWeather.764271f3.jpg"
+  },
+  {
+    "revision": "74f4a59a5f64c9a40e1ff948c05b4e3e",
+    "url": "/static/media/SRVTexasFlood.74f4a59a.jpg"
+  }
+]);
